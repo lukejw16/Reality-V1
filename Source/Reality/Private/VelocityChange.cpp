5bb30e0935ef26dc893dba8891d394bc7a47a0ca
Luke <lukejw16@hotmail.co.uk>
2019-09-25 22:42:14 +0100

    Small Update
    Velocity pads now changed to fix small issues and now keep velocity capped when going through them

--- Source/Reality/Private/VelocityChange.cpp
+++ Source/Reality/Private/VelocityChange.cpp
@@ -36,23 +36,35 @@ void AVelocityChange::Tick(float DeltaTime)
 void AVelocityChange::OnOverlapBegin(UPrimitiveComponent * OverlapComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
 	Character = Cast<ARealityCharacter>(OtherActor);
-	if (Character)
+	if (Character != nullptr)
 	{
 		const FVector ForwardDir = Character->GetRootComponent()->GetForwardVector();
 		const FVector ForwardWithUp = (Character->GetRootComponent()->GetUpVector());
 		const FVector ForwardWithZ = (ForwardDir + FVector(0, 0, 1));
-		if (bIsForward == true)
+		const FVector Velocity = Character->GetCharacterMovement()->Velocity;
+		const FVector MinVelocity(300.0f, 300.0f, 300.0f);
+		float speed = Velocity.Size();
+		FString TheFloatStr = FString::SanitizeFloat(speed);
+		GEngine->AddOnScreenDebugMessage(-1, 1.0, FColor::Red, *TheFloatStr);
+		if (bIsForward == true )
 		{
 			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Overlap"));
 			FMath::Clamp(SpeedChange, 600.0f, 2000.0f);
 			Character->GetCharacterMovement()->Velocity = ForwardDir * SpeedChange;
 		}
 
-		if (bIsPlus == true && bIsForward == false)
+		if (bIsPlus == true && bIsForward == false && speed < 3000)
 		{
 			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Overlap"));
 			FMath::Clamp(SpeedChange, 600.0f, 2000.0f);
 			Character->GetCharacterMovement()->Velocity += ForwardDir * SpeedChange;
+		}
+
+		if (bIsPlus == true && bIsForward == false && speed > 5000)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Overlap"));
+			FMath::Clamp(SpeedChange, 600.0f, 2000.0f);
+			Character->GetCharacterMovement()->Velocity = ForwardDir * SpeedChange;
 		}
 
 		if (bIsForward == false && bIsPlus == false)
@@ -62,6 +74,12 @@ void AVelocityChange::OnOverlapBegin(UPrimitiveComponent * OverlapComponent, AAc
 			Character->GetCharacterMovement()->Velocity += ForwardWithUp * SpeedChange;
 		}
 
+		if (bIsForward == false && bIsPlus == false && speed > 500)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Overlap"));
+			FMath::Clamp(SpeedChange, 600.0f, 2000.0f);
+			Character->GetCharacterMovement()->Velocity = ForwardWithUp * SpeedChange;
+		}
 	}
 	
 	
